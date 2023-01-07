e5e219e8db1b8b3df52ab4a0d0bf394088e8d49d
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-01-08 01:05:18 +0700

    interaction

--- webpage/src/App.js
+++ webpage/src/App.js
@@ -1,6 +1,7 @@
 import { insApi, batchApi, ins } from "./contractInfos";
 import { useState } from 'react'
 import { ethers } from 'ethers'
+import { QRCodeSVG } from 'qrcode.react';
 
 const connect = async () => {
   if (typeof window.ethereum) {
@@ -10,36 +11,63 @@ const connect = async () => {
   }
 }
 
+//To put floating point values onto the blockchain
+const formatNumber = (n) => ethers.utils.parseEther(n);
+
 const addBatch = async () => {
   if (typeof window.ethereum) {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
     const contract = new ethers.Contract(ins, insApi, signer)
     try {
-      await contract.addBatch("Milk", 10, 20, 30, 40, 50, 60, 70, '20-03-2002', '20-03-2100')
+      await contract.addBatch("Milk",
+        formatNumber('10'), formatNumber('20'),
+        formatNumber('30'), formatNumber('40'),
+        formatNumber('50'), formatNumber('60'),
+        formatNumber('70'), '20-03-2002', '20-03-2100')
     } catch (error) {
       console.log(error)
     }
   }
 }
-const viewBatch = async () => {
+const viewBatch = async (setCurrentBatch) => {
   if (typeof window.ethereum) {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     const signer = provider.getSigner()
     const contract = new ethers.Contract(ins, insApi, signer)
     try {
-      const batchAddress = await contract.getBatchAtIndex(0);
-      const provider2 = new ethers.providers.Web3Provider(window.ethereum)
+      const batchAddress = await contract.getBatchAtIndex(1);
       const contract2 = new ethers.Contract(batchAddress, batchApi, signer)
-      const resp = await contract2.getStat(1);
+      const resp = await contract2.getStat(0);
+      const formattedData = resp.map((n) => {
+        if (n._isBigNumber) {
+          return ethers.utils.formatEther(n)
+        }
+        return n;
+      })
       console.log(resp);
+      console.log(formattedData);
+      setCurrentBatch(formattedData);
     } catch (error) {
       console.log(error)
     }
   }
 }
 
+const batchInfoToJsonString = (batchInfo) =>
+  `{"energy":"${batchInfo[0]}",
+  "protein":"${batchInfo[1]}",
+  "carbohydrate":"${batchInfo[2]}",
+  "totalSugar":"${batchInfo[3]}",
+  "fat":"${batchInfo[4]}",
+  "saturatedFat":"${batchInfo[5]}",
+  "natri":"${batchInfo[6]}",
+  "productionDate":"${batchInfo[7]}",
+  "expiryDate":"${batchInfo[8]}",
+  "inspectedDate":"${batchInfo[9]}"}`
+
 function App() {
+  const [currentBatch, setCurrentBatch] = useState([])
   return (
     <div className="App">
       <header className="App-header">
@@ -50,7 +78,11 @@ function App() {
           <button onClick={() => addBatch()}>Create batch</button>
         </div>
         <div className="view-batch">
-          <button onClick={() => viewBatch()}>View batch</button>
+          <button onClick={() => viewBatch(setCurrentBatch)}>View batch</button>
+        </div>
+        <div className="qr-code">
+          <button>Generate QR</button>
+          <QRCodeSVG value={batchInfoToJsonString(currentBatch)} />
         </div>
       </header>
     </div>
