bf23b74c9a71f6907333cc82d24d5b69ecea8785
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-01-07 23:45:18 +0700

    solidity

--- contracts/Batch.sol
+++ contracts/Batch.sol
@@ -3,6 +3,15 @@ pragma solidity ^0.8.9;
 
 contract Batch {
     string public name;
+    // Batch item is added and assigned inspector at the same time
+    // For this development purpose, it'll be the account that create the contract
+    address public immutable inspector;
+    // Only inspector can perform certain functions (inspect)
+    modifier onlyInspector() {
+        // If the address calling this contract isn't the inspector's address, the function will stop and throw an error "You're not..."
+        require(inspector == msg.sender, "You're not privileged to do this");
+        _;
+    }
     struct foodStat {
         uint256 energy; // In Kcal / KJ
         uint256 protein; // g
@@ -15,13 +24,22 @@ contract Batch {
         string expiriyDate; //dd-mm-yyyy
         string inspectedDate; //dd-mm-yyyy suppliedStat will be initialise with 0
     }
-    foodStat public suppliedStat;
-    foodStat public inspectedStat;
+    foodStat private suppliedStat;
+    foodStat private inspectedStat;
+    //statChecks work the same as foodStat but with string for everything except for inspectedDate.
+    //In this concept, expiryDate and productionDate MUST be the same for legal problems
+    string[9] private statChecks;
+    // Max allowed difference between provided stat and inspected stat
+    uint8 public maxAllowedDiff = 1;
 
     // When getting new batch to inspect, manually insert these the original stat supplied from partner to create a new product resembling
     // the whole batch ( as a smart contract )
+
+    // From inspect contract deploy this contract
+    // Store each of these contract address into an array in Inspect contract
     constructor(
-        // i stans for input
+        // i stands for input
+        address i_inspector,
         string memory i_name,
         uint256 i_energy,
         uint256 i_protein,
@@ -29,8 +47,11 @@ contract Batch {
         uint256 i_totalSugar,
         uint256 i_fat,
         uint256 i_satFat,
-        uint256 i_natri
+        uint256 i_natri,
+        string memory i_productionDate,
+        string memory i_expiryDate
     ) {
+        inspector = i_inspector;
         name = i_name;
         suppliedStat.energy = i_energy;
         suppliedStat.protein = i_protein;
@@ -39,5 +60,109 @@ contract Batch {
         suppliedStat.fat = i_fat;
         suppliedStat.saturatedFat = i_satFat;
         suppliedStat.natri = i_natri;
+        suppliedStat.productionDate = i_productionDate;
+        suppliedStat.expiriyDate = i_expiryDate;
+        suppliedStat.inspectedDate = "Not inspected";
+    }
+
+    function getStatChecks(uint8 index) public view returns (string memory) {
+        return statChecks[index];
+    }
+
+    // If 0, returns suppliedStat
+    // If 1, returns inspectedStat
+    function getStat(uint8 statType) public view returns (foodStat memory) {
+        if (statType == 0) {
+            return suppliedStat;
+        } else return inspectedStat;
+    }
+
+    //onlyInspector is the modifier for only assigned inspector (station) is allowed to do this, declared at top of contract
+    function inspect(
+        uint256 i_energy,
+        uint256 i_protein,
+        uint256 i_carbohydrate,
+        uint256 i_totalSugar,
+        uint256 i_fat,
+        uint256 i_satFat,
+        uint256 i_natri,
+        string memory i_productionDate,
+        string memory i_expiryDate,
+        string memory i_inspectedDate
+    ) public onlyInspector {
+        inspectedStat.energy = i_energy;
+        inspectedStat.protein = i_protein;
+        inspectedStat.carbohydrate = i_carbohydrate;
+        inspectedStat.totalSugar = i_totalSugar;
+        inspectedStat.fat = i_fat;
+        inspectedStat.saturatedFat = i_satFat;
+        inspectedStat.natri = i_natri;
+        inspectedStat.productionDate = i_productionDate;
+        inspectedStat.expiriyDate = i_expiryDate;
+        inspectedStat.inspectedDate = i_inspectedDate;
+        //Check if provided stat are up to actual stat
+        //Loop from the first index to the 7th item in statChecks
+        //Energy check
+        statChecks[0] = checkIfUnderMaxAllowance(
+            suppliedStat.energy,
+            inspectedStat.energy
+        );
+        //Protein check
+        statChecks[1] = checkIfUnderMaxAllowance(
+            suppliedStat.protein,
+            inspectedStat.protein
+        );
+        //carbohydrate check
+        statChecks[2] = checkIfUnderMaxAllowance(
+            suppliedStat.carbohydrate,
+            inspectedStat.carbohydrate
+        );
+        //totalSugar check
+        statChecks[3] = checkIfUnderMaxAllowance(
+            suppliedStat.totalSugar,
+            inspectedStat.totalSugar
+        );
+        //totalSugar check
+        statChecks[4] = checkIfUnderMaxAllowance(
+            suppliedStat.fat,
+            inspectedStat.fat
+        );
+        //saturatedFat check
+        statChecks[5] = checkIfUnderMaxAllowance(
+            suppliedStat.saturatedFat,
+            inspectedStat.saturatedFat
+        );
+        //natri check
+        statChecks[6] = checkIfUnderMaxAllowance(
+            suppliedStat.natri,
+            inspectedStat.natri
+        );
+        //productionDate check
+        //keccak256 is a hashing function
+        //Solidity can't directly compare strings so this is one workaround
+        statChecks[7] = keccak256(
+            abi.encodePacked(suppliedStat.productionDate)
+        ) == keccak256(abi.encodePacked(inspectedStat.productionDate))
+            ? "Pass"
+            : "Alert";
+        //expiryDate check
+        statChecks[8] = keccak256(abi.encodePacked(suppliedStat.expiriyDate)) ==
+            keccak256(abi.encodePacked(inspectedStat.expiriyDate))
+            ? "Pass"
+            : "Alert";
+    }
+
+    function checkIfUnderMaxAllowance(
+        uint256 supplied,
+        uint256 inspected
+    ) private view returns (string memory) {
+        if (supplied > inspected) {
+            if (supplied - inspected <= maxAllowedDiff) return "Pass";
+            else return "Alert";
+        } else {
+            // If Insppected <= Supplied will make the difference be at most 0 and not negative
+            if (inspected - supplied <= maxAllowedDiff) return "Pass";
+            else return "Alert";
+        }
     }
 }
--- contracts/Inspect.sol
+++ contracts/Inspect.sol
@@ -1,4 +1,83 @@
 // SPDX-License-Indentifier: MIT
 pragma solidity ^0.8.9;
+import "./Batch.sol";
 
-contract Inspector {}
+contract Inspector {
+    address public immutable inspector;
+
+    constructor() {
+        inspector = msg.sender;
+    }
+
+    //Store items that are added but not inspected
+    address[] private batchesToInspect;
+    //Store items that are inspected
+    address[] private inspectedBatches;
+
+    function addBatch(
+        string memory i_name,
+        uint256 i_energy,
+        uint256 i_protein,
+        uint256 i_carbohydrate,
+        uint256 i_totalSugar,
+        uint256 i_fat,
+        uint256 i_satFat,
+        uint256 i_natri,
+        string memory i_productionDate,
+        string memory i_expiryDate
+    ) public {
+        // Create a new batch with given inputs
+        Batch newBatch = new Batch(
+            inspector,
+            i_name,
+            i_energy,
+            i_protein,
+            i_carbohydrate,
+            i_totalSugar,
+            i_fat,
+            i_satFat,
+            i_natri,
+            i_productionDate,
+            i_expiryDate
+        );
+        // Push newly added item into itemsToInspect array
+        batchesToInspect.push(address(newBatch));
+    }
+
+    function inspect(
+        address batchToInspect,
+        uint256 i_energy,
+        uint256 i_protein,
+        uint256 i_carbohydrate,
+        uint256 i_totalSugar,
+        uint256 i_fat,
+        uint256 i_satFat,
+        uint256 i_natri,
+        string memory i_productionDate,
+        string memory i_expiryDate,
+        string memory i_inspectedDate
+    ) public {
+        //Create a Batch contract from address passed into function
+        Batch inspectingBatch = Batch(batchToInspect);
+        //Perform inspect
+        inspectingBatch.inspect(
+            i_energy,
+            i_protein,
+            i_carbohydrate,
+            i_totalSugar,
+            i_fat,
+            i_satFat,
+            i_natri,
+            i_productionDate,
+            i_expiryDate,
+            i_inspectedDate
+        );
+        //Push batch to inspected batches
+        inspectedBatches.push(batchToInspect);
+    }
+
+    //Get batch at specified index
+    function getBatchAtIndex(uint8 index) public view returns (address) {
+        return batchesToInspect[index];
+    }
+}
