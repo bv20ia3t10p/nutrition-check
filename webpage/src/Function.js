102078f6170e8b07f5d1d79ed0f778469ed56247
Phạm Trọng Phúc <94206242+bv20ia3t10p@users.noreply.github.com>
2023-01-13 02:39:04 -0800

    Update Function.js

--- webpage/src/Function.js
+++ webpage/src/Function.js
@@ -45,8 +45,8 @@ export const getListOfItemsToInspect = async (setBatchesToInspect) => {
 export const formatNumber = (n) => ethers.utils.parseEther(`${n}`)
 export const sysDatetoString = () => {
   const today = new Date()
-  const dd = String(today.getDate()).padStart(2, '0')
-  const mm = String(today.getMonth() + 1).padStart(2, '0') //January is 0!
+  const dd = String(today.getDate())
+  const mm = String(today.getMonth() + 1) //January is 0!
   const yyyy = today.getFullYear()
   const inspectDate = mm + '-' + dd + '-' + yyyy
 
